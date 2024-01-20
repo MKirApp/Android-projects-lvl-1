24ed7c318ec590157b3741dbd8f0dbdc5632e853
Kirill <mkir2001@mail.ru>
2024-01-21 01:12:24 +0700

    Added Json annotations to the UsefulActivityDto.kt

--- ActivityForDay/app/src/main/java/com/applicaton/activityforday/data/UsefulActivityDto.kt
+++ ActivityForDay/app/src/main/java/com/applicaton/activityforday/data/UsefulActivityDto.kt
@@ -1,14 +1,22 @@
 package com.applicaton.activityforday.data
 
 import com.applicaton.activityforday.entity.UsefulActivity
+import com.squareup.moshi.Json
 
 
-data class UsefulActivityDto (
+data class UsefulActivityDto(
+    @Json(name = "activity")
     override val activity: String,
+    @Json(name = "type")
     override val type: String,
+    @Json(name = "participants")
     override val participants: Int,
+    @Json(name = "price")
     override val price: Double,
+    @Json(name = "link")
     override val link: String,
+    @Json(name = "key")
     override val key: String,
+    @Json(name = "accessibility")
     override val accessibility: Double
 ) : UsefulActivity
